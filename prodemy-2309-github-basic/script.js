ddec2b5b962925c616ae90792b05920133f1b948
Ramadhan Rizqi Saputra <RamadhanRzq@gmail.com>
2023-11-16 10:27:41 +0700

    Push Pertama

---
+++ prodemy-2309-github-basic/script.js
@@ -0,0 +1,7 @@
+document.getElementById("openModal").addEventListener("click", function() {
+    document.getElementById("cartModal").style.display = "block";
+});
+
+document.getElementById("closeModal").addEventListener("click", function() {
+    document.getElementById("cartModal").style.display = "none";
+});
