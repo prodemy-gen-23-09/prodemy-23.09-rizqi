33722c92bd70ed56738b9e8917705ac2da85cdb3
Ramadhan Rizqi Saputra <55622076+RamadhanRzq@users.noreply.github.com>
2023-11-27 16:30:46 +0700

    update script.js

--- gen-23.09-javascript-basic/src/script.js
+++ gen-23.09-javascript-basic/src/script.js
@@ -1,4 +1,31 @@
-function redirectToProductDetail(productId, productName, productPrice,productImage) {
-    var productDetailUrl = 'detail_product1.html?id=' + productId + '&name=' + encodeURIComponent(productName) + '&price=' + encodeURIComponent(productPrice) + '&image=' + encodeURIComponent(productImage);
-    window.location.href = productDetailUrl;
-  }
+function redirectToProductDetail(
+  productId,
+  productName,
+  productPrice,
+  productImage
+) {
+  var productDetailUrl =
+    "detail_product1.html?id=" +
+    productId +
+    "&name=" +
+    encodeURIComponent(productName) +
+    "&price=" +
+    encodeURIComponent(productPrice) +
+    "&image=" +
+    encodeURIComponent(productImage);
+  window.location.href = productDetailUrl;
+}
+
+function changeMainImage(newImagePath) {
+  document.getElementById("mainImage").src = newImagePath;
+}
+
+var urlParams = new URLSearchParams(window.location.search);
+var productId = urlParams.get("id");
+var productName = urlParams.get("name");
+var productPrice = urlParams.get("price");
+var productImage = urlParams.get("image");
+
+document.getElementById("productName").innerText = productName;
+document.getElementById("productPrice").innerText = productPrice;
+document.getElementById("mainImage").src = productImage;
