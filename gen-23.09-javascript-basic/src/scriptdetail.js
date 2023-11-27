b482db28a793b0fcc959d52c732e3da1740429d3
Ramadhan Rizqi Saputra <55622076+RamadhanRzq@users.noreply.github.com>
2023-11-27 16:29:46 +0700

    update button

--- gen-23.09-javascript-basic/src/scriptdetail.js
+++ gen-23.09-javascript-basic/src/scriptdetail.js
@@ -1,13 +1,3 @@
-function selectButton(buttonId) {
-  const buttons = document.querySelectorAll("button");
-  buttons.forEach((button) => {
-    button.classList.remove("bg-color_selected");
-  });
-
-  const selectedButton = document.getElementById(buttonId);
-  selectedButton.classList.add("bg-color_selected");
-}
-
 function changeMainImage(newImagePath) {
   document.getElementById("mainImage").src = newImagePath;
 }
