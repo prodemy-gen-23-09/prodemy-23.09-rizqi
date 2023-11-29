f8b96b01141871826c6acea27faad77f001f9549
Ramadhan Rizqi Saputra <55622076+RamadhanRzq@users.noreply.github.com>
2023-11-29 17:01:43 +0700

    Update Layout

--- gen-23.09-react-basic/src/App.jsx
+++ gen-23.09-react-basic/src/App.jsx
@@ -1,19 +1,16 @@
 import BannerImage from "./components/BannerImage.jsx";
 import FilterAndSort from "./components/FilterAndSort.jsx";
 import BannerService from "./components/BannerService.jsx";
-import Header from "./layout/Header.jsx";
-import Footer from "./layout/Footer.jsx";
+
 import ListProduct from "./layout/ListProduct";
 
 function App() {
   return (
     <div>
-      <Header />
       <BannerImage />
       <FilterAndSort />
       <ListProduct/>
       <BannerService />
-      <Footer />
     </div>
   );
 }
