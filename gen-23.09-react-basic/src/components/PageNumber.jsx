1655406a5b270dbe938fd9f4cd20cead25168cfa
Ramadhan Rizqi Saputra <55622076+RamadhanRzq@users.noreply.github.com>
2023-11-29 17:02:08 +0700

    Update PageNumber.jsx

--- gen-23.09-react-basic/src/components/PageNumber.jsx
+++ gen-23.09-react-basic/src/components/PageNumber.jsx
@@ -3,8 +3,7 @@ function PageNumber(props) {
   return (
     <div>
         <button
-          className="bg-color1 hover:bg-color1_selected duration-1000 w-12 h-14 text-center flex items-center justify-center rounded-md"
-        >
+          className="bg-color1 hover:bg-color1_selected duration-1000 w-12 h-14 text-center flex items-center justify-center rounded-md">
           <p>{props.pageNumber}</p>
         </button>
       </div>
