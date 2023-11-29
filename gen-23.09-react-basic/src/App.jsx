2d655176e37ddf6b30aae2e83c46aca7b81a73a2
Ramadhan Rizqi Saputra <55622076+RamadhanRzq@users.noreply.github.com>
2023-11-29 15:44:50 +0700

    Update card

--- gen-23.09-react-basic/src/App.jsx
+++ gen-23.09-react-basic/src/App.jsx
@@ -2,71 +2,83 @@ import Card from "./components/Card";
 import PageNumber from "./components/PageNumber";
 
 function App() {
+  const FurnitureProducts = [
+    {
+      id: 1,
+      image: "/src/assets/shop/product1.png",
+      title: "Syltherine",
+      desc: "Stylish cafe table",
+      price: "Rp 2.500.000",
+    },
+    {
+      id: 2,
+      image: "/src/assets/shop/product2.png",
+      title: "Asgard Sofa",
+      desc: "Stylish cafe sofa",
+      price: "Rp 2.500.000",
+    },
+    {
+      id: 3,
+      image: "/src/assets/shop/product3.png",
+      title: "Potty",
+      desc: "Stylish cafe chair",
+      price: "Rp 2.500.000",
+    },
+    {
+      id: 4,
+      image: "/src/assets/shop/product4.png",
+      title: "Respira",
+      desc: "Stylish cafe chair",
+      price: "Rp 2.500.000",
+    },
+    {
+      id: 5,
+      image: "/src/assets/shop/product5.png",
+      title: "Lolito",
+      desc: "Stylish cafe chair",
+      price: "Rp 2.500.000",
+    },
+    {
+      id: 6,
+      image: "/src/assets/shop/product6.png",
+      title: "Leviosa",
+      desc: "Stylish sofa",
+      price: "Rp 2.500.000",
+    },
+    {
+      id: 7,
+      image: "/src/assets/shop/product7.png",
+      title: "Flowy",
+      desc: "Stylish Flower Decoration",
+      price: "Rp 2.500.000",
+    },
+    {
+      id: 8,
+      image: "/src/assets/shop/product8.png",
+      title: "Muggo",
+      desc: "Stylish Mug",
+      price: "Rp 2.500.000",
+    },
+  ];
+  console.log(FurnitureProducts)
   return (
     <div>
       <div className="grid grid-cols-4">
-        <Card
-          id="1"
-          image="/src/assets/shop/product1.png"
-          title="Asgard Sofa"
-          desc="Stylish cafe chair"
-          price="Rp 2.500.000"
-        />
-        <Card
-          id="2"
-          image="/src/assets/shop/product2.png"
-          title="Asgard Sofa"
-          desc="Stylish cafe chair"
-          price="Rp 2.500.000"
-        />
-        <Card
-          id="3"
-          image="/src/assets/shop/product3.png"
-          title="Asgard Sofa"
-          desc="Stylish cafe chair"
-          price="Rp 2.500.000"
-        />
-        <Card
-          id="4"
-          image="/src/assets/shop/product4.png"
-          title="Asgard Sofa"
-          desc="Stylish cafe chair"
-          price="Rp 2.500.000"
-        />
-        <Card
-          id="5"
-          image="/src/assets/shop/product5.png"
-          title="Asgard Sofa"
-          desc="Stylish cafe chair"
-          price="Rp 2.500.000"
-        />
-        <Card
-          id="6"
-          image="/src/assets/shop/product6.png"
-          title="Asgard Sofa"
-          desc="Stylish cafe chair"
-          price="Rp 2.500.000"
-        />
-        <Card
-          id="7"
-          image="/src/assets/shop/product7.png"
-          title="Asgard Sofa"
-          desc="Stylish cafe chair"
-          price="Rp 2.500.000"
-        />
-        <Card
-          id="8"
-          image="/src/assets/shop/product8.png"
-          title="Asgard Sofa"
-          desc="Stylish cafe chair"
-          price="Rp 2.500.000"
-        />
+      {FurnitureProducts.map(product => (
+          <Card
+            key={product.id}
+            image={product.image}
+            title={product.title}
+            desc={product.desc}
+            price={product.price}
+          />
+        ))}
       </div>
       <div className="flex justify-center items-center my-14 gap-10">
-        <PageNumber pageNumber="1"/>
-        <PageNumber pageNumber="2"/>
-        <PageNumber pageNumber="3"/>
-        <PageNumber pageNumber="Next"/>
+        <PageNumber pageNumber="1" />
+        <PageNumber pageNumber="2" />
+        <PageNumber pageNumber="3" />
+        <PageNumber pageNumber="Next" />
       </div>
     </div>
   );
--- gen-23.09-react-basic/src/components/Card.jsx
+++ gen-23.09-react-basic/src/components/Card.jsx
@@ -3,7 +3,7 @@ function Card(props) {
   return (
     <div>
       <div
-        className="hover:bg-color1_selected transition-all duration-1000 ease-out transform hover:opacity-80 text-center mx-16 mt-12 w-80 h-[450px] cursor-pointer rounded-md shadow-md"
+        className="hover:bg-color1_selected transition-all duration-1000 ease-out transform hover:opacity-80 text-center mx-16 mt-12 w-80 h-[450px] cursor-pointer rounded-md shadow-lg"
         id={props.id}
       >
         <img
