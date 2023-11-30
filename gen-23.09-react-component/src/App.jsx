fe0b113bfa30b29d05414cc468d5bd0e5591e5e3
Ramadhan Rizqi Saputra <55622076+RamadhanRzq@users.noreply.github.com>
2023-11-30 10:25:33 +0700

    update project

--- gen-23.09-react-component/src/App.jsx
+++ gen-23.09-react-component/src/App.jsx
@@ -1,7 +1,6 @@
 import BannerImage from "./components/BannerImage.jsx";
 import FilterAndSort from "./components/FilterAndSort.jsx";
 import BannerService from "./components/BannerService.jsx";
-
 import ListProduct from "./layout/ListProduct";
 
 function App() {
