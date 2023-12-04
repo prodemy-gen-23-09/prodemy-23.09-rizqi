ae5da6d190f8a2f65013acbb1ce556ca7f4ebb26
Ramadhan Rizqi Saputra <55622076+RamadhanRzq@users.noreply.github.com>
2023-12-04 22:49:22 +0700

    Update ModalsImage.jsx

--- gen-23.09-react-useeffect/src/components/Modals/ModalsImage.jsx
+++ gen-23.09-react-useeffect/src/components/Modals/ModalsImage.jsx
@@ -6,17 +6,13 @@ import React, { useState } from "react";
 function ModalsImage({ closeModal, image }) {
   return (
     <>
-      <div className="modal-box">
-        <form method="dialog">
-          <button
-            className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
-            onClick={closeModal}
-          >
-            ✕
-          </button>
-        </form>
-        <img src={image} className="w-full h-full" />
-      </div>
+      <button
+        className="btn btn-sm bg-white btn-circle btn-ghost absolute right-[405px] top-2"
+        onClick={closeModal}
+      >
+        ✕
+      </button>
+      <img src={image} className="bg-white w-[1000px] h-[850px]" />
     </>
   );
 }
