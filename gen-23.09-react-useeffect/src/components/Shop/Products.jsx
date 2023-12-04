5cc228f0a22feae1fc1507902e77e38aa726b9de
Ramadhan Rizqi Saputra <55622076+RamadhanRzq@users.noreply.github.com>
2023-12-04 15:28:37 +0700

    Done Task Sorting Products

--- gen-23.09-react-useeffect/src/components/Shop/Products.jsx
+++ gen-23.09-react-useeffect/src/components/Shop/Products.jsx
@@ -37,43 +37,12 @@ function Products() {
   }, [sortOption]);
   return (
     <>
-      <div className="bg-color1 justify-between items-center flex w-auto h-16">
-        <div className="function-tools flex items-center mx-24">
-          <img
-            src="/src/assets/shop/filter.png"
-            alt="filter"
-            className="mr-10 max-w-8 max-h-8"
-          />
-          <p className="mr-10">Filters</p>
-          <img
-            src="/src/assets/shop/grid.png"
-            alt="grid"
-            className="mr-10 max-w-8 max-h-8"
-          />
-          <img
-            src="/src/assets/shop/viewlist.png"
-            alt="viewlist"
-            className="mr-10 max-w-8 max-h-8"
-          />
-          <img
-            src="/src/assets/shop/line.png"
-            alt="line"
-            className="mr-10 max-w-8 max-h-8"
-          />
+      <div className="bg-color1 justify-end items-center flex w-auto h-16">
+        <div className="px-88 function-tools-filter flex items-center mx-24 gap-10">
           <p>
-            Showing 1-{FurnitureProducts.length} of {FurnitureProducts.length}{" "}
+            Showing 1 - {FurnitureProducts.length} of {FurnitureProducts.length}{" "}
             results
           </p>
-        </div>
-        <div className="px-88 function-tools-filter flex items-center mx-24">
-          <p className="mr-2">Show</p>
-          <input
-            type="text"
-            name="inputData"
-            id="inputData"
-            placeholder="8"
-            className="mr-8 w-40 h-8"
-          />
           <Dropdown onSort={handleSort} />
         </div>
       </div>
