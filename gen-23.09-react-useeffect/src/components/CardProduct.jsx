c848cecc3ee33a8e29dfd049106253647fc06f23
Ramadhan Rizqi Saputra <55622076+RamadhanRzq@users.noreply.github.com>
2023-12-05 10:39:56 +0700

    Update CardProduct.jsx

--- gen-23.09-react-useeffect/src/components/CardProduct.jsx
+++ gen-23.09-react-useeffect/src/components/CardProduct.jsx
@@ -6,14 +6,15 @@ function CardProduct(props) {
         className="hover:bg-color1_selected transition-all duration-1000 ease-out transform hover:opacity-80 text-center mx-16 mt-12 w-80 h-[450px] cursor-pointer rounded-md shadow-lg"
         id={props.id}
       >
+        <div className="absolute text-gray-800 right-2 top-2">{props.date}</div>
         <img
           src={props.image}
           alt="imgproduct"
           className="rounded-md w-full h-3/4"
         />
         <h1 className="my-2 text-3xl font-semibold">{props.title}</h1>
+        <p className="mb-2">{props.desc}</p>
         <p className="mb-2">{props.price}</p>
-        <p className="mb-2">Release Date : {props.date}</p>
       </div>
     </div>
   );
