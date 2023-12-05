3acdb3da85e8990aa5f2b254f1e6c42c76406789
Ramadhan Rizqi Saputra <55622076+RamadhanRzq@users.noreply.github.com>
2023-12-05 11:37:46 +0700

    init folder react-routing

---
+++ gen-23.09-react-routing/src/components/CardProduct.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable react/prop-types */
+function CardProduct(props) {
+  return (
+    <div>
+      <div
+        className="hover:bg-color1_selected transition-all duration-1000 ease-out transform hover:opacity-80 text-center mx-16 mt-12 w-80 h-[450px] cursor-pointer rounded-md shadow-lg"
+        id={props.id}
+      >
+        <div className="absolute text-gray-800 right-2 top-2">{props.date}</div>
+        <img
+          src={props.image}
+          alt="imgproduct"
+          className="rounded-md w-full h-3/4"
+        />
+        <h1 className="my-2 text-3xl font-semibold">{props.title}</h1>
+        <p className="mb-2">{props.desc}</p>
+        <p className="mb-2">{props.price}</p>
+      </div>
+    </div>
+  );
+}
+
+export default CardProduct;
---
+++ gen-23.09-react-routing/src/pages/About.jsx
@@ -0,0 +1,11 @@
+import BannerImage from "../components/BannerImage";
+
+function About() {
+  return (
+    <div>
+      <BannerImage title="About" title2="Home > About" />
+    </div>
+  );
+}
+
+export default About;
