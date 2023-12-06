d8df1db4d79af0c81d90cb2111fbaa77eb37e20e
Ramadhan Rizqi Saputra <55622076+RamadhanRzq@users.noreply.github.com>
2023-12-06 08:53:30 +0700

    Update Layout.jsx

--- gen-23.09-react-useeffect/src/pages/Layout.jsx
+++ gen-23.09-react-useeffect/src/pages/Layout.jsx
@@ -1,13 +1,11 @@
 import Header from "../layout/Header";
 import Footer from "../layout/Footer";
-import { Outlet } from "react-router-dom";
 
 function Layout() {
   return (
     <>
       <Header />
       <Footer />
-      <Outlet />
     </>
   );
 }
