abed8adb8feb6cdee24f3000e052b15772fb6dce
Ramadhan Rizqi Saputra <55622076+RamadhanRzq@users.noreply.github.com>
2023-12-06 16:33:59 +0700

    Add Method Search Product With Axios

--- gen-23.09-javascript-asynchronous/script.js
+++ gen-23.09-javascript-asynchronous/script.js
@@ -1,3 +1,32 @@
+// function searchProduct() {
+//   var searchTerm = document.getElementById("searchInput").value;
+
+//   var apiUrl = "https://dummyjson.com/products/search?q=" + searchTerm;
+
+//   fetch(apiUrl)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       console.log(data);
+//     })
+//     .catch((error) => {
+//       console.error(error);
+//     });
+// }
+
+function searchProduct() {
+  var searchTerm = document.getElementById("searchInput").value;
+  let url = "https://dummyjson.com/products/search?q=" + searchTerm;
+
+  axios
+    .get(url)
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
 document
   .getElementById("searchInput")
   .addEventListener("keydown", function (event) {
@@ -5,18 +34,3 @@ document
       searchProduct();
     }
   });
-
-function searchProduct() {
-  var searchTerm = document.getElementById("searchInput").value;
-
-  var apiUrl = "https://dummyjson.com/products/search?q=" + searchTerm;
-
-  fetch(apiUrl)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-}
