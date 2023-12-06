7dcabb1986864a9d755a9b9a009f304c8830caaa
Ramadhan Rizqi Saputra <55622076+RamadhanRzq@users.noreply.github.com>
2023-12-06 16:49:24 +0700

    Add List For Product

--- gen-23.09-javascript-asynchronous/script.js
+++ gen-23.09-javascript-asynchronous/script.js
@@ -13,18 +13,41 @@
 //     });
 // }
 
-const searchProduct = async () => {
-  var searchTerm = document.getElementById("searchInput").value;
-  let url = "https://dummyjson.com/products/search?q=" + searchTerm;
+// const searchProduct = async () => {
+//   var searchTerm = document.getElementById("searchInput").value;
+//   let url = "https://dummyjson.com/products/search?q=" + searchTerm;
 
-  axios
-    .get(url)
-    .then((response) => {
-      console.log(response.data);
-    })
-    .catch((error) => {
-      console.error(error);
+//   axios
+//     .get(url)
+//     .then((response) => {
+//       console.log(response.data);
+//     })
+//     .catch((error) => {
+//       console.error(error);
+//     });
+// };
+
+const searchProduct = async () => {
+  try {
+    var searchTerm = document.getElementById("searchInput").value;
+    var apiUrl = "https://dummyjson.com/products/search?q=" + searchTerm;
+
+    const response = await axios.get(apiUrl);
+
+    document.getElementById("productTitleList").innerHTML = "";
+
+    const productTitleList = document.getElementById("productTitleList");
+
+    console.log(response.data.products); //Hasil Di Console
+
+    response.data.products.forEach((product) => {
+      const listItem = document.createElement("li");
+      listItem.textContent = product.title;
+      productTitleList.appendChild(listItem);
     });
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 document
