ff8381eae3d5ad666a54ead7846d128bbdd56aab
Ramadhan Rizqi Saputra <55622076+RamadhanRzq@users.noreply.github.com>
2023-12-07 16:01:51 +0700

    Update DetailProduct.jsx

--- gen-23.09-react-fetch-api/src/pages/DetailProduct.jsx
+++ gen-23.09-react-fetch-api/src/pages/DetailProduct.jsx
@@ -1,24 +1,42 @@
 import Breadcrumb from "../components/Shop/Breadcrumb";
 import { useParams } from "react-router-dom";
-import { useState } from "react";
-import FurnitureProducts from "../data/data";
+import { useState, useEffect } from "react";
 import ImageThumbnail from "../components/DetailProduct/ImageThumbnail";
 import ProductContent from "../components/DetailProduct/ProductContent";
 import Information from "../components/DetailProduct/Information";
 import OverlayImage from "../components/Modals/OverlayImage";
+import { getAllProduct } from "../service/api";
 
 function DetailProduct() {
   const { id } = useParams();
-  const product = FurnitureProducts.find(
-    (product) => product.id === parseInt(id)
-  );
-
-  const [mainImage, setMainImage] = useState(product.image);
+  const [products, setProducts] = useState([]);
+  const [detailProducts, setDetailProducts] = useState(null);
+  const [mainImage, setMainImage] = useState("");
   const [isModalImageOpen, setModalImageOpen] = useState(false);
+
+  const data = async () => {
+    try {
+      const result = await getAllProduct();
+      setProducts(result);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useState(() => {
+    data();
+  }, []);
+
+  useEffect(() => {
+    const detail = products.find((p) => p.id === parseInt(id));
+    setDetailProducts(detail || null);
+    setMainImage(detail?.thumbnail || "");
+  }, [id, products]);
 
   const openModalImage = () => {
     setModalImageOpen(true);
   };
+
   const closeModal = () => {
     setModalImageOpen(false);
   };
@@ -26,26 +44,18 @@ function DetailProduct() {
   const handleThumbnailClick = (newImage) => {
     setMainImage(newImage);
   };
-  const formatPrice = (price) => {
-    return price.toLocaleString("id-ID", {
-      style: "currency",
-      currency: "IDR",
-    });
-  };
-  const formatDate = (date) => {
-    return date.toLocaleDateString("en-US", {
-      year: "numeric",
-      month: "long",
-      day: "numeric",
-    });
-  };
+
+  if (!detailProducts) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <>
-      <Breadcrumb title={product.title} />
+      <Breadcrumb title={detailProducts.title} />
       <div className="flex bg-white mt-8 mb-10">
         <div className="flex flex-col gap-10 ml-24">
           <ImageThumbnail
-            image={product.image}
+            image={detailProducts.thumbnail}
             onThumbnailClick={handleThumbnailClick}
           />
         </div>
@@ -58,15 +68,18 @@ function DetailProduct() {
           </div>
         </div>
         <ProductContent
-          title={product.title}
-          price={formatPrice(product.price)}
-          desc={product.desc}
-          category={product.category}
-          date={formatDate(product.release_date)}
+          title={detailProducts.title}
+          price={detailProducts.price}
+          desc={detailProducts.desc}
+          category={detailProducts.category}
+          date={detailProducts.release_date}
         />
       </div>
       <hr />
-      <Information image1={product.image} image2={product.image} />
+      <Information
+        image1={detailProducts.thumbnail}
+        image2={detailProducts.thumbnail}
+      />
       <hr />
       {isModalImageOpen && (
         <OverlayImage closeModal={closeModal} image={mainImage} />
