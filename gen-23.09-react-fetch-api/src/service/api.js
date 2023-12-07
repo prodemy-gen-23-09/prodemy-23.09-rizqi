3104fef06e4a0f5549e421cb416c52a1031a5860
Ramadhan Rizqi Saputra <55622076+RamadhanRzq@users.noreply.github.com>
2023-12-07 11:51:09 +0700

    Create service api and db.json

--- gen-23.09-react-fetch-api/src/service/api.js
+++ gen-23.09-react-fetch-api/src/service/api.js
@@ -1,3 +1,8 @@
 import axios from "axios";
 
 const url = "http://localhost:3000/products";
+
+export const getAllProduct = async () => {
+  const response = await axios.get(url);
+  return response.data;
+};
