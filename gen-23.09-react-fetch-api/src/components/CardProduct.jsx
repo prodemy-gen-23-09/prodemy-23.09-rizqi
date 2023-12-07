c552e33ad0d6e5196704228853ac7e4ba189c4cb
Ramadhan Rizqi Saputra <55622076+RamadhanRzq@users.noreply.github.com>
2023-12-07 23:24:43 +0700

    Update CardProduct.jsx

--- gen-23.09-react-fetch-api/src/components/CardProduct.jsx
+++ gen-23.09-react-fetch-api/src/components/CardProduct.jsx
@@ -1,21 +1,17 @@
 /* eslint-disable react/prop-types */
-function CardProduct(props) {
+function CardProduct({ id, image, title, date, desc, price, onClick }) {
   return (
     <div>
       <div
         className="hover:bg-color1_selected transition-all duration-1000 ease-out transform hover:opacity-80 text-center mx-16 mt-12 w-80 h-[450px] cursor-pointer rounded-md shadow-lg"
-        id={props.id}
-        onClick={props.onClick}
+        id={id}
+        onClick={onClick}
       >
-        <div className="absolute text-gray-800 right-2 top-2">{props.date}</div>
-        <img
-          src={props.image}
-          alt="imgproduct"
-          className="rounded-md w-full h-3/4"
-        />
-        <h1 className="my-2 text-3xl font-semibold">{props.title}</h1>
-        <p className="mb-2">{props.desc}</p>
-        <p className="mb-2">{props.price}</p>
+        <div className="absolute text-gray-800 right-2 top-2">{date}</div>
+        <img src={image} alt="imgproduct" className="rounded-md w-full h-3/4" />
+        <h1 className="my-2 text-3xl font-semibold">{title}</h1>
+        <p className="mb-2">{desc}</p>
+        <p className="mb-2">{price}</p>
       </div>
     </div>
   );
