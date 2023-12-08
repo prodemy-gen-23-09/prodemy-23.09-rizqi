832d54de40a952ef0d510f152ca99c15f335c336
Ramadhan Rizqi Saputra <55622076+RamadhanRzq@users.noreply.github.com>
2023-12-08 10:53:59 +0700

    Create Layout Admin

--- gen-23.09-react-form-handling/src/admin/components/TableAdmin.jsx
+++ gen-23.09-react-form-handling/src/admin/components/TableAdmin.jsx
@@ -6,11 +6,14 @@ function TableAdmin(props) {
       <table className="table-fixed w-full">
         <thead className="bg-color_home h-16">
           <tr>
-            <th>Product</th>
+            <th>Title</th>
             <th>Price</th>
-            <th>Quantity</th>
-            <th>Subtotal</th>
-            <th>Subtotal</th>
+            <th>Stock</th>
+            <th>Image</th>
+            <th>Thumbnail</th>
+            <th>Category</th>
+            <th>Desc</th>
+            <th>Release_Date</th>
           </tr>
         </thead>
         <tbody>
@@ -18,12 +21,12 @@ function TableAdmin(props) {
             <td>
               <div className="flex items-center justify-center gap-6">
                 <img src={props.image} width={120} />
-                <p>{props.title}</p>
               </div>
             </td>
             <td>{props.price}</td>
             <td>{props.quantity}</td>
             <td>{props.subtotal}</td>
+            <td>{props.title}</td>
           </tr>
         </tbody>
       </table>
--- gen-23.09-react-form-handling/src/admin/layout/HeaderAdmin.jsx
+++ gen-23.09-react-form-handling/src/admin/layout/HeaderAdmin.jsx
@@ -8,7 +8,7 @@ function HeaderAdmin() {
     <div>
       <header>
         <nav className="flex font-medium bg-white justify-between mt-2  h-20 sticky top-0 items-center mx-[54px] p-[10px]">
-          <Link to="/">
+          <Link to="/admin">
             <img
               src="/src/assets/logo.png"
               alt="imglogo"
@@ -16,19 +16,8 @@ function HeaderAdmin() {
             />
           </Link>
           <div className="flex gap-20 cursor-pointer">
-            <Link to="/">
+            <Link to="/admin">
               <div className="text-black hover:text-color1_selected">Home</div>
-            </Link>
-            <Link to="/shop">
-              <div className="text-black hover:text-color1_selected">Shop</div>
-            </Link>
-            <Link to="/about">
-              <div className="text-black hover:text-color1_selected">About</div>
-            </Link>
-            <Link to="/contact">
-              <div className="text-black hover:text-color1_selected">
-                Contact
-              </div>
             </Link>
           </div>
           <div className="flex m-5 gap-5">
