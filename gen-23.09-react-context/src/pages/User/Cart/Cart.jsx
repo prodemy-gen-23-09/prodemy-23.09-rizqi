9a4493d59cf8f9e3754e10f553c03193e3d6f83e
Ramadhan Rizqi Saputra <55622076+RamadhanRzq@users.noreply.github.com>
2023-12-11 16:44:02 +0700

    Done Task React-Context :100:

--- gen-23.09-react-context/src/pages/User/Cart/Cart.jsx
+++ gen-23.09-react-context/src/pages/User/Cart/Cart.jsx
@@ -20,14 +20,14 @@ function Cart() {
       <BannerImage title="Cart" />
       <div className="flex my-20 mx-[100px] justify-between">
         <Table cartItems={cartItems} />
-        <div className="flex flex-col bg-color_home w-[400px] h-[400px] p-8 rounded-sm shadow-lg">
-          <div className="flex flex-col">
+        <div className="flex flex-col bg-color_home w-[400px] h-[400px] p-8 rounded-sm shadow-lg ">
+          <div className="flex flex-col justify-center items center">
             <p className="text-3xl font-bold mx-auto">Cart Totals</p>
-            <p className="text-xl mt-32">
+            <p className="text-xl mt-32 mx-auto">
               Total : {formatPrice(getCartTotal())}
             </p>
             <Link to="/checkout">
-              <button className="bg-color1_selected hover:bg-color3 mt-24 rounded-sm shadow-lg w-52 h-10 mx-auto font-semibold ">
+              <button className="flex bg-color1_selected hover:bg-color3 mt-24 rounded-md shadow-lg w-52 h-10 mx-auto text-white hover:text-black justify-center items-center">
                 Checkout
               </button>
             </Link>
