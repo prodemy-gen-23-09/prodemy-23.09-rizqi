45a89a4eee49f0a9b766ba8b518f5b46f1c8c5bf
Ramadhan Rizqi Saputra <55622076+RamadhanRzq@users.noreply.github.com>
2023-12-18 09:15:03 +0700

    Update TableCart.jsx

--- gen-23.09-react-cart-api/src/components/Admin/TableCart.jsx
+++ gen-23.09-react-cart-api/src/components/Admin/TableCart.jsx
@@ -4,20 +4,12 @@
 import Button from "./Button";
 
 export default function TableCart({ cart }) {
-  const date2 = new Date();
-  const generateTransactionNumber = (date) => {
-    const formattedDate = new Date(date).toISOString().replace(/[-T:.Z]/g, "");
-    return `TRN.${formattedDate}`;
-  };
-
-  console.log(date2);
   return (
     <>
       <table className="table-fixed w-full">
         <thead className="bg-color_home h-16 w-full">
           <tr>
             <th>No</th>
-            <th>No. Transaction</th>
             <th>Date</th>
             <th>Email</th>
             <th>Username</th>
@@ -32,7 +24,6 @@ export default function TableCart({ cart }) {
                 onClick={() => console.log("Detail")}
               >
                 <td>{index + 1}</td>
-                <td>{generateTransactionNumber(date2)}</td>
                 <td>{cart.date}</td>
                 <td>{cart.email}</td>
                 <td>{cart.username}</td>
