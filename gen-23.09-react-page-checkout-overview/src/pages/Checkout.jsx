9031d41bbbb30495b596f9140dc87c9706234a1e
Ramadhan Rizqi Saputra <55622076+RamadhanRzq@users.noreply.github.com>
2023-12-18 17:08:27 +0700

    Update Page Checkout

--- gen-23.09-react-page-checkout-overview/src/pages/Checkout.jsx
+++ gen-23.09-react-page-checkout-overview/src/pages/Checkout.jsx
@@ -1,104 +1,15 @@
 import BannerImage from "../components/User/BannerImage";
+import BillingDetails from "./BillingDetails";
+import ProductCheckout from "./ProductCheckout";
 
-function Checkout() {
+export default function Checkout() {
   return (
     <>
       <BannerImage title="Checkout" />
-      <div className="flex flex-col mx-[175px] my-[100px] w-[530px] gap-10">
-        <h1 className="text-3xl font-bold">Billing Details</h1>
-        <div className="flex gap-8">
-          <label className="form-control w-[250px]">
-            <div className="label">
-              <span className="label-text text-md font-semibold">
-                First Name
-              </span>
-            </div>
-            <input
-              name="email"
-              id="email"
-              type="email"
-              className="input input-bordered w-full"
-              autoComplete="email"
-            />
-          </label>
-          <label className="form-control w-[250px]">
-            <div className="label">
-              <span className="label-text text-md font-semibold">
-                Last Name
-              </span>
-            </div>
-            <input
-              name="email"
-              id="email"
-              type="email"
-              className="input input-bordered w-full"
-              autoComplete="email"
-            />
-          </label>
-        </div>
-        <label className="form-control w-full">
-          <div className="label">
-            <span className="label-text text-md font-semibold">Last Name</span>
-          </div>
-          <input
-            name="email"
-            id="email"
-            type="email"
-            className="input input-bordered w-full"
-            autoComplete="email"
-          />
-        </label>
-        <label className="form-control w-full">
-          <div className="label">
-            <span className="label-text text-md font-semibold">Last Name</span>
-          </div>
-          <input
-            name="email"
-            id="email"
-            type="email"
-            className="input input-bordered w-full"
-            autoComplete="email"
-          />
-        </label>
-        <label className="form-control w-full">
-          <div className="label">
-            <span className="label-text text-md font-semibold">Last Name</span>
-          </div>
-          <input
-            name="email"
-            id="email"
-            type="email"
-            className="input input-bordered w-full"
-            autoComplete="email"
-          />
-        </label>
-        <label className="form-control w-full">
-          <div className="label">
-            <span className="label-text text-md font-semibold">Last Name</span>
-          </div>
-          <input
-            name="email"
-            id="email"
-            type="email"
-            className="input input-bordered w-full"
-            autoComplete="email"
-          />
-        </label>
-        <label className="form-control w-full">
-          <div className="label">
-            <span className="label-text text-md font-semibold">Last Name</span>
-          </div>
-          <input
-            name="email"
-            id="email"
-            type="email"
-            className="input input-bordered w-full"
-            autoComplete="email"
-          />
-        </label>
+      <div className="flex">
+        <BillingDetails />
+        <ProductCheckout />
       </div>
     </>
   );
 }
-
-export default Checkout;
