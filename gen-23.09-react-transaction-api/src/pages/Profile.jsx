8ca48fbedf7f6954815a8f5c37e017854bc73a4d
Ramadhan Rizqi Saputra <55622076+RamadhanRzq@users.noreply.github.com>
2023-12-19 22:26:34 +0700

    Update Profile.jsx

--- gen-23.09-react-transaction-api/src/pages/Profile.jsx
+++ gen-23.09-react-transaction-api/src/pages/Profile.jsx
@@ -7,29 +7,11 @@ import axios from "axios";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { useForm } from "react-hook-form";
-import { getAllUsers } from "../service/users";
+import { mutate } from "swr";
 
 export default function Profile() {
-  const schema = yup.object().shape({
-    username: yup.string().required("Name is required"),
-    address: yup.string().required("Address is required"),
-    postalcode: yup.string().required("Postal Code is required"),
-    phone: yup.string().required("Phone is required"),
-    email: yup.string().required("Email is required"),
-    password: yup.string().required("Password is required"),
-  });
-
   const user = useSelector((state) => state.auth.user);
-  const { mutate } = getAllUsers(user.id);
-  const {
-    register,
-    handleSubmit,
-    setValue,
-    formState: { errors },
-  } = useForm({
-    resolver: yupResolver(schema),
-  });
-
+  const { register, handleSubmit, setValue } = useForm();
   const [dataProfile, setDataProfile] = useState([]);
 
   useEffect(() => {
@@ -46,7 +28,6 @@ export default function Profile() {
         setValue("postalcode", userProfileData.postalcode);
         setValue("phone", userProfileData.phone);
         setValue("email", userProfileData.email);
-        setValue("password", userProfileData.password);
 
         setDataProfile(userProfileData);
       } catch (error) {
@@ -55,28 +36,43 @@ export default function Profile() {
     };
     mutate();
     fetchProfileData();
-  }, [setValue, user, mutate]);
+  }, [setValue, user]);
 
-  const onSubmitForm = async (data) => {
+  const { username, address, postalcode, phone, email } = dataProfile || {};
+
+  useEffect(() => {
+    setValue("username", username || "");
+    setValue("address", address || "");
+    setValue("postalcode", postalcode || "");
+    setValue("phone", phone || "");
+    setValue("email", email || "");
+  }, [setValue, username, address, postalcode, phone, email]);
+
+  const submitForm = async (data) => {
+    console.log(data);
     try {
-      const { password, ...updatedProfileData } = data;
+      const updatedProductData = {
+        username: data.username,
+        address: data.address,
+        postalcode: data.postalcode,
+        phone: data.phone,
+        email: data.email,
+      };
 
       await axios
-        .put(`http://localhost:3000/users/${user.id}`, updatedProfileData)
+        .put(`http://localhost:3000/users/${user.id}`, updatedProductData)
         .then(() => {
-          alert("Profile updated successfully!");
+          alert("Success Edit Users!");
         });
     } catch (error) {
-      console.error("Error updating profile:", error);
+      console.error("Error updating Users:", error);
     }
+    mutate(`http://localhost:3000/users/${user.id}`);
   };
-
-  console.log(dataProfile);
-
   return (
     <>
       <BannerImage title="Profile" />
-      <form onSubmit={handleSubmit(onSubmitForm)}>
+      <form onSubmit={handleSubmit(submitForm)}>
         <div className="flex mx-48 shadow-xl p-32 gap-6 ">
           <div className="grid grid-cols-2 gap-80">
             <div className="flex flex-col ">
@@ -93,8 +89,8 @@ export default function Profile() {
                   defaultValue={dataProfile.username}
                   {...register("username")}
                 />
-                <p className="error text-red-600">{errors.username?.message}</p>
               </label>
+
               <label className="form-control w-[450px]">
                 <div className="label">
                   <span className="label-text text-md font-semibold">
@@ -109,7 +105,6 @@ export default function Profile() {
                   defaultValue={dataProfile.address}
                   {...register("address")}
                 />
-                <p className="error text-red-600">{errors.address?.message}</p>
               </label>
               <label className="form-control w-[450px]">
                 <div className="label">
@@ -126,9 +121,7 @@ export default function Profile() {
                   defaultValue={dataProfile.postalcode}
                   {...register("postalcode")}
                 />
-                <p className="error text-red-600">
-                  {errors.postalcode?.message}
-                </p>
+                <p className="error text-red-600"></p>
               </label>
               <label className="form-control w-[450px]">
                 <div className="label">
@@ -145,7 +138,6 @@ export default function Profile() {
                   defaultValue={dataProfile.phone}
                   {...register("phone")}
                 />
-                <p className="error text-red-600">{errors.phone?.message}</p>
               </label>
               <label className="form-control w-[450px]">
                 <div className="label">
@@ -162,7 +154,6 @@ export default function Profile() {
                   defaultValue={dataProfile.email}
                   {...register("email")}
                 />
-                <p className="error text-red-600">{errors.email?.message}</p>
               </label>
             </div>
             <div className="flex flex-col ">
