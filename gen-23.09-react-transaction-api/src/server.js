e4d9248559f2424f4c83ed2edf9562de3ca4cd4c
Ramadhan Rizqi Saputra <55622076+RamadhanRzq@users.noreply.github.com>
2023-12-20 11:20:05 +0700

    update server

--- gen-23.09-react-transaction-api/src/server.js
+++ gen-23.09-react-transaction-api/src/server.js
@@ -1,8 +1,38 @@
-// server.js
 import jsonServer from "json-server";
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
+
+server.get("/product", (req, res) => {
+  const products = router.db.get("products").value();
+  res.jsonp(products);
+});
+
+server.get("/transactions", (req, res) => {
+  const transactions = router.db.get("transaction").value();
+  res.jsonp(transactions);
+});
+
+server.get("/users", (req, res) => {
+  const users = router.db.get("users").value();
+  res.jsonp(users);
+});
+
+server.get("/cart", (req, res) => {
+  const cart = router.db.get("cart").value();
+  res.jsonp(cart);
+});
+
+server.post("/cart", (req, res) => {
+  const newCartItem = req.body;
+  const cart = router.db.get("cart").push(newCartItem).write();
+  res.jsonp(cart);
+});
+
+server.get("/checkout", (req, res) => {
+  const checkout = router.db.get("checkout").value();
+  res.jsonp(checkout);
+});
 
 server.use((req, res, next) => {
   if (req.method === "DELETE" && req.query.userId) {
